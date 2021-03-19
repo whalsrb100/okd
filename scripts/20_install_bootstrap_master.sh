b731e9abc5ffdca4bf1e065664a02aad71986bad
chominkyu <whalsrb100@gmail.com>
2021-03-19 14:26:26 +0900

    20210319

--- scripts/20_install_bootstrap_master.sh
+++ scripts/20_install_bootstrap_master.sh
@@ -7,15 +7,15 @@ echo -e "\n${BOOTSTRAP_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
 echo -ne "Wait for bootstrap health check"
 while [ $(ssh core@${BOOTSTRAP_HOSTNAME}.${ClusterName}.${DomainName} "journalctl -b | grep 'healthy: successfully committed proposal'" 2> /dev/null | wc -l) -ne 1 ];do sleep 5;echo -n ".";done
 
-echo "Please Install \"${MASTER1_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
+echo -e \n"Please Install \"${MASTER1_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${MASTER1_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${MASTER1_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${MASTER1_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
 
-echo "Please Install \"${MASTER2_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
+echo -e "\nPlease Install \"${MASTER2_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${MASTER2_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${MASTER2_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${MASTER2_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
 
-echo "Please Install \"${MASTER3_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
+echo -e "\nPlease Install \"${MASTER3_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${MASTER3_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${MASTER3_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${MASTER3_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
 
