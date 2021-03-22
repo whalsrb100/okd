2d77c134c2f5d92c17fbc6e9c342c50c72dd6ec1
mj <54505914+whalsrb100@users.noreply.github.com>
2021-03-22 17:56:46 +0900

    Update 02_install_pkgs.sh

--- scripts/02_install_pkgs.sh
+++ scripts/02_install_pkgs.sh
@@ -8,7 +8,7 @@ fi
 
 LOG_FILE="${0}_log"
 dnf install -y tar bzip2 gzip xz jq git rsync vim net-tools sos sysstat bind bind-utils podman httpd-tools \
-	dhcp-server tftp-server httpd syslinux libvirt-libs wget haproxy expect bc | tee ${LOG_FILE}
+	dhcp-server tftp-server httpd syslinux libvirt-libs wget haproxy expect bc chrony | tee ${LOG_FILE}
 
 systemctl enable --now io.podman.service io.podman.socket podman.socket
 systemctl disable dhcpd tftp.socket
