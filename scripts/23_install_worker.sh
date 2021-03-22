6c134ee47969e821314b9bd83f476b88bdc49c75
mj <54505914+whalsrb100@users.noreply.github.com>
2021-03-22 11:38:41 +0900

    Update 23_install_worker.sh

--- scripts/23_install_worker.sh
+++ scripts/23_install_worker.sh
@@ -13,16 +13,16 @@ source src/env
 # 7 : worker4
 ################################################
 
-function default_count_add() {
-sed -i "s/prompt\s.*/prompt ${1}/" /var/lib/tftpboot/pxelinux.cfg/default
+function modify_default() {
+sed -i "s/default\s.*/default ${1}/" /var/lib/tftpboot/pxelinux.cfg/default
 }
 
-default_count_add "4"
+modify_default ${WORKER1_HOSTNAME}
 echo "Please Install \"${WORKER1_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${WORKER1_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${WORKER1_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${WORKER1_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
 
-default_count_add "5"
+modify_default ${WORKER2_HOSTNAME}
 echo "Please Install \"${WORKER2_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${WORKER2_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${WORKER2_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${WORKER2_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
