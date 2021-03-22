8b5aecae13dd57b456d8d754af15ea13973ccb5a
mj <54505914+whalsrb100@users.noreply.github.com>
2021-03-22 11:37:57 +0900

    Update 21_install_bootstrap_master.sh

--- scripts/21_install_bootstrap_master.sh
+++ scripts/21_install_bootstrap_master.sh
@@ -14,28 +14,28 @@ source src/env
 # 7 : worker4
 ################################################
 
-function default_count_add() {
-sed -i "s/prompt\s.*/prompt ${1}/" /var/lib/tftpboot/pxelinux.cfg/default
+function modify_default() {
+sed -i "s/default\s.*/default ${1}/" /var/lib/tftpboot/pxelinux.cfg/default
 }
 
-default_count_add "0"
+modify_default ${BOOTSTRAP_HOSTNAME}
 echo "Please Install \"${BOOTSTRAP_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${BOOTSTRAP_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${BOOTSTRAP_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${BOOTSTRAP_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
 echo -ne "Wait for bootstrap health check"
 while [ $(ssh core@${BOOTSTRAP_HOSTNAME}.${ClusterName}.${DomainName} "journalctl -b | grep 'healthy: successfully committed proposal'" 2> /dev/null | wc -l) -ne 1 ];do sleep 5;echo -n ".";done
 
-default_count_add "1"
+modify_default ${MASTER1_HOSTNAME}
 echo -e \n"Please Install \"${MASTER1_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${MASTER1_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${MASTER1_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${MASTER1_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
 
-default_count_add "2"
+modify_default ${MASTER2_HOSTNAME}
 echo -e "\nPlease Install \"${MASTER2_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${MASTER2_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${MASTER2_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${MASTER2_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
 
-default_count_add "3"
+modify_default ${MASTER3_HOSTNAME}
 echo -e "\nPlease Install \"${MASTER3_HOSTNAME}.${ClusterName}.${DomainName}\" Node"
 while [ "$(ssh core@${MASTER3_HOSTNAME}.${ClusterName}.${DomainName} 'hostname' 2> /dev/null)" != "${MASTER3_HOSTNAME}.${ClusterName}.${DomainName}" ];do sleep 10;echo -n ".";done
 echo -e "\n${MASTER3_HOSTNAME}.${ClusterName}.${DomainName} Installed !"
