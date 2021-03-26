08e1a11fc1d0748713e47fa002fe34442d840ffc
mj <54505914+whalsrb100@users.noreply.github.com>
2021-03-26 09:22:46 +0900

    Update 20_add_chrony_ignitionconfigs.sh

--- scripts/20_add_chrony_ignitionconfigs.sh
+++ scripts/20_add_chrony_ignitionconfigs.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 source src/env
+
+# Set Timezone : Asia/Seoul
+SetTimeZone="Asia/Seoul"
+
+BOOTSTRAP_JQ="${OKD_HOME}/jq_bootstrap.ign"
+WORKER_JQ="${OKD_HOME}/jq_worker.ign"
+MASTER_JQ="${OKD_HOME}/jq_master.ign"
+
 cp -a ${OKD_HOME}/bootstrap.ign ${OKD_HOME}/bootstrap.ign_backup
 cp -a ${OKD_HOME}/worker.ign ${OKD_HOME}/worker.ign_backup
 cp -a ${OKD_HOME}/master.ign ${OKD_HOME}/master.ign_backup
 
-cat ${OKD_HOME}/bootstrap.ign | jq '.' > ${OKD_HOME}/jq_bootstrap.ign
-cat ${OKD_HOME}/worker.ign | jq '.' > ${OKD_HOME}/jq_worker.ign
-cat ${OKD_HOME}/master.ign | jq '.' > ${OKD_HOME}/jq_master.ign
+cat ${OKD_HOME}/bootstrap.ign | jq '.' > ${BOOTSTRAP_JQ}
+cat ${OKD_HOME}/worker.ign | jq '.' > ${MASTER_JQ}
+cat ${OKD_HOME}/master.ign | jq '.' > ${WORKER_JQ}
 
-sed -i "/\"files\":/a\{\"overwrite\":true,\"path\":\"\/etc\/chrony.conf\",\"user\":{\"name\":\"root\"},\"contents\":{\"source\":\"data:text/plain;charset=utf-8;base64,$(echo "$(cat ${HTTP_HOME}/chrony.conf)" | base64 -w0)\"},\"mode\":420}," ${OKD_HOME}/jq_bootstrap.ign
-sed -i "/^}/i\,\"storage\":{\"files\":[{\"overwrite\":true,\"path\":\"\/etc\/chrony.conf\",\"user\":{\"name\":\"root\"},\"contents\":{\"source\":\"data:text/plain;charset=utf-8;base64,$(echo "$(cat ${HTTP_HOME}/chrony.conf)" | base64 -w0)\"},\"mode\":420}]}" ${OKD_HOME}/jq_worker.ign
-sed -i "/^}/i\,\"storage\":{\"files\":[{\"overwrite\":true,\"path\":\"\/etc\/chrony.conf\",\"user\":{\"name\":\"root\"},\"contents\":{\"source\":\"data:text/plain;charset=utf-8;base64,$(echo "$(cat ${HTTP_HOME}/chrony.conf)" | base64 -w0)\"},\"mode\":420}]}" ${OKD_HOME}/jq_master.ign
 
-cat ${OKD_HOME}/jq_bootstrap.ign | jq -c '.' > ${OKD_HOME}/bootstrap.ign
-cat ${OKD_HOME}/jq_worker.ign | jq -c '.' > ${OKD_HOME}/worker.ign
-cat ${OKD_HOME}/jq_master.ign | jq -c '.' > ${OKD_HOME}/master.ign
+sed -i "/\"files\":/a\{\"overwrite\":true,\"path\":\"\/etc\/chrony.conf\",\"user\":{\"name\":\"root\"},\"contents\":{\"source\":\"data:text/plain;charset=utf-8;base64,$(echo "$(cat ${HTTP_HOME}/chrony.conf)" | base64 -w0)\"},\"mode\":420}," ${BOOTSTRAP_JQ}
+sed -i "$(expr $(cat -n ${BOOTSTRAP_JQ} | grep \"mode\" | tail -n1 | awk '{print $1}') \+ 2)a\,\"links\":[{\"overwrite\":true,\"path\":\"\/etc\/localtime\",\"user\":{\"name\":\"root\"},\"group\":{\"name\":\"root\"},\"target\":\"..\/usr\/share\/zoneinfo\/$(echo ${SetTimeZone} | sed 's/\//\\\//g')\",\"hard\":false}]" ${BOOTSTRAP_JQ}
 
-rm -f ${OKD_HOME}/jq_bootstrap.ign
-rm -f ${OKD_HOME}/jq_worker.ign
-rm -f ${OKD_HOME}/jq_master.ign
+sed -i "/^}/i\,\"storage\":{\"files\":[{\"overwrite\":true,\"path\":\"\/etc\/chrony.conf\",\"user\":{\"name\":\"root\"},\"contents\":{\"source\":\"data:text/plain;charset=utf-8;base64,$(echo "$(cat ${HTTP_HOME}/chrony.conf)" | base64 -w0)\"},\"mode\":420}],\"links\": [{\"overwrite\":true,\"path\":\"/etc/localtime\",\"user\":{\"name\":\"root\"},\"group\":{\"name\":\"root\"},\"target\":\"../usr/share/zoneinfo/Asia/Seoul\",\"hard\":false}]}" ${WORKER_JQ}
+sed -i "/^}/i\,\"storage\":{\"files\":[{\"overwrite\":true,\"path\":\"\/etc\/chrony.conf\",\"user\":{\"name\":\"root\"},\"contents\":{\"source\":\"data:text/plain;charset=utf-8;base64,$(echo "$(cat ${HTTP_HOME}/chrony.conf)" | base64 -w0)\"},\"mode\":420}],\"links\": [{\"overwrite\":true,\"path\":\"/etc/localtime\",\"user\":{\"name\":\"root\"},\"group\":{\"name\":\"root\"},\"target\":\"../usr/share/zoneinfo/Asia/Seoul\",\"hard\":false}]}" ${MASTER_JQ}
+
+cat ${BOOTSTRAP_JQ} | jq -c '.' > ${OKD_HOME}/bootstrap.ign
+cat ${WORKER_JQ} | jq -c '.' > ${OKD_HOME}/worker.ign
+cat ${MASTER_JQ} | jq -c '.' > ${OKD_HOME}/master.ign
+
+rm -f ${BOOTSTRAP_JQ}
+rm -f ${WORKER_JQ}
+rm -f ${MASTER_JQ}
 
 
 chown apache. -R ${HTTP_HOME}
