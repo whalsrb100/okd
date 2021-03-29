0bf661184c658b387da7aa9b26843050a5ee59f6
mj <54505914+whalsrb100@users.noreply.github.com>
2021-03-29 17:54:45 +0900

    Update and rename 02_install_pkgs.sh to 01_install_pkgs.sh

--- scripts/01_install_pkgs.sh
+++ scripts/01_install_pkgs.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 source src/env
 
+sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/*
+cat << EOF > /etc/yum.repos.d/local.repo
+[AppStream_iso]
+name=AppStream_iso
+baseurl=file:///media/AppStream
+gpgcheck=0
+enabled=1
+
+[BaseOS_iso]
+name=BaseOS_iso
+baseurl=file:///media/BaseOS
+gpgcheck=0
+enabled=1
+EOF
+
 if [ $(df -P | awk '{if($NF=="/media")print}' | wc -l) -eq 0 ];then
   echo 'You need to Mount CentOS-8.3 image on "/media".'
   exit 1
@@ -8,7 +23,7 @@ fi
 
 LOG_FILE="${0}_log"
 dnf install -y tar bzip2 gzip xz jq git rsync vim net-tools sos sysstat bind bind-utils podman httpd-tools \
-	dhcp-server tftp-server httpd syslinux libvirt-libs wget haproxy expect bc chrony | tee ${LOG_FILE}
+	dhcp-server tftp-server httpd syslinux libvirt-libs wget haproxy expect bc | tee ${LOG_FILE}
 
 systemctl enable --now io.podman.service io.podman.socket podman.socket
 systemctl disable dhcpd tftp.socket
