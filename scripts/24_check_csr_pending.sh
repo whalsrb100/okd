eec08762be71d84a2590bc2a0f10ea8ffca89158
mj <54505914+whalsrb100@users.noreply.github.com>
2021-03-29 18:37:36 +0900

    Update 24_check_csr_pending.sh

--- scripts/24_check_csr_pending.sh
+++ scripts/24_check_csr_pending.sh
@@ -2,8 +2,13 @@
 source src/env
 export KUBECONFIG=${OKD_HOME}/auth/kubeconfig
 
+rm -f csr_count
 Approve_count=0
-while [ ${Approve_count} -lt $(expr ${Approve_count} \* 2) ];do
-Approve_count=$(expr ${Approve_count} \+ $(oc get csr | grep Pending | awk '{print "oc adm certificate approve "$1}' | wc -l))
-oc get csr | grep Pending | awk '{print "oc adm certificate approve "$1}'| sh
+while [ ${Approve_count} -lt $(expr ${WORKER_NUM} \* 2) ];do
+  oc get csr | grep Pending | awk '{print "oc adm certificate approve "$1}' >> csr_count
+  cat csr_count | sort -Vu > csr_count.tmp
+  cat csr_count.tmp > csr_count
+  cat csr_count | sh 2> /dev/null
+  if [ $(cat csr_count | wc -l) -ge 4 ];then break;fi
 done
+rm -f csr_count
