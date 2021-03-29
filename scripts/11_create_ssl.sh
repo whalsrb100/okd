2d486353f2501963a606010f7216041493da6c43
mj <54505914+whalsrb100@users.noreply.github.com>
2021-03-29 18:33:26 +0900

    Update 11_create_ssl.sh

--- scripts/11_create_ssl.sh
+++ scripts/11_create_ssl.sh
@@ -7,7 +7,7 @@ rm -f /etc/pki/ca-trust/source/anchors/{$(basename ${KEY}),$(basename ${CRT})} $
 -x509 -days 3650 \
 -keyout ${KEY} \
 -out ${CRT} \
--subj "/C=KR/ST=Seoul/L=City/O=SelfSign/OU=Cloud/CN=*.${ClusterName}.${DomainName}/emailAddress=${E_MAIL}"
+-subj "/C=KR/ST=Seoul/L=City/O=SelfSign/OU=Cloud/CN=${BASTION_HOSTNAME}.${ClusterName}.${DomainName}/emailAddress=${E_MAIL}"
 ls -l $CRT $KEY
 
 /usr/bin/cp -a ${KEY} ${CRT} /etc/pki/ca-trust/source/anchors/
