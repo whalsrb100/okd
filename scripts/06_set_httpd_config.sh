4078cc3ee13f941bf4d9c6c1409e2d91c635f60f
mj <54505914+whalsrb100@users.noreply.github.com>
2021-03-31 21:45:36 +0900

    Update 06_set_httpd_config.sh

--- scripts/06_set_httpd_config.sh
+++ scripts/06_set_httpd_config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source src/env
-unlink /html > /dev/null 2>&1
-ln -s var/www/html /html
+ln -sf var/www/html /html
 sed -i 's/^Listen 80$/Listen 8080/' /etc/httpd/conf/httpd.conf
 systemctl enable --now httpd
